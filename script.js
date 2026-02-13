d26d4d64f0263e802ff356ab29f47a36f28e9940
Prerna Malhotra <prernamalhotra997@gmail.com>
2026-02-13 19:48:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const imageUpload = document.getElementById('imageUpload');
+const imagePreview = document.getElementById('imagePreview');
+const placeholderText = document.getElementById('placeholderText');
+...
